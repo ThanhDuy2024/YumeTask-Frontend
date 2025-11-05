c01f93156cff648ada31962c9653a761926ca989
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2025-11-05 19:46:06 +0700

    [Update] profile route

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,4 +1,5 @@
 import { LoginPage } from '@/pages/auth/LoginPage'
+import { ProfilePage } from '@/pages/auth/ProfilePage'
 import { RegisterPage } from '@/pages/auth/RegisterPage'
 import { TaskPage } from '@/pages/task/TaskPage'
 import { Route, Routes } from 'react-router-dom'
@@ -9,6 +10,7 @@ export const AppRouter = () => {
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
         <Route path='/task' element={<TaskPage />} />
+        <Route path='/profile' element={<ProfilePage/>}/>
       </Routes>
     </>
   )
