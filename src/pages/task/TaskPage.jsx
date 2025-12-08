4c4a0b62fddc9a481f519d73e7974776670afe5a
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2025-12-08 09:29:25 +0700

    [Update] pagination

--- src/pages/task/TaskPage.jsx
+++ src/pages/task/TaskPage.jsx
@@ -1,6 +1,8 @@
 import AddTask from "@/components/addTask";
 import StatsAndFilters from "@/components/statsAndFilters";
 import TaskList from "@/components/taskList";
+import TaskListPagination from "@/components/taskListPagination";
+import { limit } from "@/lib/data";
 import { taskList } from "@/services/tasks/taskListService";
 import { useEffect } from "react";
 import { useState } from "react";
@@ -8,10 +10,15 @@ import { useState } from "react";
 export const TaskPage = () => {
   const [taskBuffer, setTaskBuffer] = useState([]);
   const [filter, setFilter] = useState("all");
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     taskApi();
   }, [])
+
+  useEffect(() => {
+    setPage(1);
+  }, [filter]);
 
   const taskApi = async () => {
     try {
@@ -26,7 +33,23 @@ export const TaskPage = () => {
   const handleTaskChange = async () => {
     await taskApi();
   }
-  
+
+  const handleNext = () => {
+    if (page < totalPages) {
+      setPage((prev) => prev + 1);
+    }
+  };
+
+  const handlePrev = () => {
+    if (page > 1) {
+      setPage((prev) => prev - 1);
+    }
+  };
+
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+  };
+
   //filter task
   const filterTasks = taskBuffer.filter((task) => {
     switch (filter) {
@@ -39,6 +62,17 @@ export const TaskPage = () => {
     }
   });
 
+  const visibleTasks = filterTasks.slice(
+    (page - 1) * limit,
+    page * limit
+  );
+
+  if (visibleTasks.length === 0) {
+    handlePrev();
+  }
+
+  const totalPages = Math.ceil(filterTasks.length / limit);
+
   return (
     <>
       {/* Your Content/Components */}
@@ -46,7 +80,7 @@ export const TaskPage = () => {
         <div className="w-full max-w-2xl p-6 mx-auto space-y-6">
           <div className="text-center text-[32px] font-700 text-[#185ADB]">Hãy bắt đầu tạo nhiệm vụ nào!!!</div>
           {/* addTask */}
-          <AddTask 
+          <AddTask
             handleNewTaskAdded={handleTaskChange}
           />
 
@@ -58,9 +92,20 @@ export const TaskPage = () => {
 
           {/* taskList */}
           <TaskList
-            filteredTasks={filterTasks}
+            filteredTasks={visibleTasks}
             handleTaskChanged={handleTaskChange}
           />
+
+          {/* Phân Trang và Lọc Theo Date */}
+          <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+            <TaskListPagination
+              handleNext={handleNext}
+              handlePrev={handlePrev}
+              handlePageChange={handlePageChange}
+              page={page}
+              totalPages={totalPages}
+            />
+          </div>
         </div>
       </div>
     </>
